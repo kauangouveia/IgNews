4a567912dec8528d5dfef38b08a4d0dd018eed44
kauan.gouveia <kauangouveia64@gmail.com>
2022-08-04 22:28:46 -0300

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,28 @@
+import { FaGithub } from 'react-icons/fa'
+import styles from './styles.module.scss';
+import { FiX } from 'react-icons/fi'
+
+
+export const SignInButton = () => {
+
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+        <button
+            type="button"
+            className={styles.signInButton}
+        >
+            <FaGithub color='#04D361' />
+            Kauan Gouveia Monte
+            <FiX color='#737380' className={styles.closeIcon} />
+        </button>
+    ) : (
+        <button
+            type="button"
+            className={styles.signInButton}
+        >
+            <FaGithub color='#EBA417' />
+            Sign in with Github
+        </button>
+    )
+}
