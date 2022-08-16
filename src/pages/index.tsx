06361b2fe5616228672f9c33174c0aab6f9fb2f5
kauan.gouveia <kauangouveia64@gmail.com>
2022-08-16 10:26:46 -0300

    fix: correct name of function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, GetStaticProps } from 'next'
+import { GetStaticProps } from 'next'
 import styles from './home.module.scss'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1LXEB1GXPwoCz16itUKmmCZL')
 
